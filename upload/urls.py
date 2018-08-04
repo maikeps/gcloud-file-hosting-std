be389c920ce7f5eb8f23c1a6e37883329b2a9964
Maike de Paula Santos <psmaike@gmail.com>
2018-08-03 21:11:25 -0300

    Simple file upload system (only shows the filename)

--- upload/urls.py
+++ upload/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'upload'
 urlpatterns = [
-	url('', views.index, name='index')
+	url(r'^$', views.index, name='index'),
+	url('file/', views.file, name='file'),
 ]
--- upload/views.py
+++ upload/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
     return render(request, 'upload/index.html')
+
+def file(request):
+    return HttpResponse('Uploaded filename: %s' % request.FILES['file'].name)
